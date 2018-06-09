5d0b62d9b33fb09d8b84fc5f51775875f1caf26d
Robert Peele <robert.s.peele@gmail.com>
2018-06-08 21:11:38 -0400

    WIP: code to parse a config file for job setup.

---
+++ Fingerator/Config.fs
@@ -0,0 +1,49 @@
+﻿module Fingerator.Config
+open FSharp.Data.UnitSystems.SI.UnitSymbols
+open CNCFingers
+
+let defaultJob =
+    {   Tool =
+            {   Diameter = 0.0<m>
+                StepOver = 0.2
+                DepthOfCut = 0.0<m>
+                FeedRate = 0.0<m/s>
+                PlungeRate = 0.0<m/s>
+                RampFactor = 2.0
+            }
+        Board =
+            {   Width = 0.0<m>
+                Thickness = 0.0<m>
+            }
+        Finger =
+            {   Count = 0
+                SideAllowance = 0.004 * 0.0254<m>
+                EndAllowance = 0.004 * 0.0254<m>
+            }
+        Start = FingerThenPocket
+    }
+
+let validateJob (job : JobParameters) =
+    let tool = job.Tool
+    let board = job.Board
+    let finger = job.Finger
+    if tool.Diameter <= 0.0<m> || tool.Diameter >= 0.25<m> then
+        Some "Tool diameter unspecified or out of range"
+    elif tool.StepOver <= 0.0 || tool.StepOver > 1.0 then
+        Some "Tool step-over is out of range (should be between 0 and 1)"
+    elif tool.RampFactor <= 0.0 || tool.RampFactor > 10.0 then
+        Some "Tool ramp factor is out of range (should be between 0 and 10)"
+    elif tool.DepthOfCut <= 0.0<m> || tool.DepthOfCut >= tool.Diameter * 2.0 then
+        Some "Depth of cut unspecified or out of range"
+    elif tool.FeedRate <= 0.0<m/s> then
+        Some "Feed rate unspecified or out of range"
+    elif tool.PlungeRate <= 0.0<m/s> || tool.PlungeRate > tool.FeedRate then
+        Some "Plunge rate unspecified or exceeds feed rate"
+    elif board.Width <= 0.0<m> then
+        Some "Board width unspecified or out of range"
+    elif board.Thickness <= 0.0<m> then
+        Some "Board thickness unspecified or out of range"
+    elif finger.Count <= 0 || float finger.Count > round (board.Width / tool.Diameter) then
+        Some "Finger count unspecified or out of range"
+    else
+        None
---
+++ Fingerator/Parsing.fs
@@ -0,0 +1,110 @@
+﻿module Fingerator.Parsing
+open FSharp.Data.UnitSystems.SI.UnitSymbols
+open CNCFingers
+open FParsec
+
+module private Guts =
+    type DimensionalTransform = float -> float<m>
+    type TimeTransform = float -> float<s>
+    type Parser<'x> = Parser<'x, unit>
+
+    module Units =
+        let inch = 0.0254<m>
+        let thou = inch * 0.001
+        let minute = 60.0<s>
+        let mm = 0.001<m>
+        let cm = 0.01<m>
+
+    let inches : Parser<DimensionalTransform> =
+        choice
+            [   pstringCI "inches"
+                pstringCI "inch"
+                pstringCI "in"
+                pstringCI "\""
+                pstringCI "freedom units"
+            ]
+        >>% (*) Units.inch
+
+    let thousandths : Parser<DimensionalTransform> =
+        choice
+            [   pstringCI "thousandth" .>> optional (pstringCI "s")
+                pstringCI "thou"
+            ]
+        >>% (*) Units.thou
+
+    let millimeters : Parser<DimensionalTransform> =
+        choice
+            [   pstringCI "millimeter" .>> optional (pstringCI "s")
+                pstringCI "millimetre" .>> optional (pstringCI "s")
+                pstringCI "mm"
+            ]
+        >>% (*) Units.mm
+
+    let centimeters : Parser<DimensionalTransform> =
+        choice
+            [   pstringCI "centimeter" .>> optional (pstringCI "s")
+                pstringCI "centimetre" .>> optional (pstringCI "s")
+                pstringCI "cm"
+            ]
+        >>% (*) Units.cm
+
+    let meters : Parser<DimensionalTransform> =
+        choice
+            [   pstringCI "meter" .>> optional (pstringCI "s")
+                pstringCI "metre" .>> optional (pstringCI "s")
+                pstringCI "m"
+            ]
+        >>% (*) 1.0<m>
+
+    let dimensionalTransform =
+        choice
+            [   inches
+                thousandths
+                millimeters
+                centimeters
+                meters
+            ]
+
+    let seconds : Parser<TimeTransform> =
+        choice
+            [   pstringCI "second" .>> optional (pstringCI "s")
+                pstringCI "s"
+            ]
+        >>% (*) 1.0<s>
+
+    let minutes : Parser<TimeTransform> =
+        choice
+            [   pstringCI "minute" .>> optional (pstringCI "s")
+                pstringCI "min" .>> optional (pstringCI "s")
+            ]
+        >>% (*) Units.minute
+
+    let timeTransform =
+        choice
+            [   seconds
+                minutes
+            ]
+
+    let ws = spaces
+    let ws1 = spaces1
+    let number = pfloat
+
+    let distance =
+        pipe2
+            (number .>> ws)
+            dimensionalTransform
+            (|>)
+
+    let speed =
+        pipe2
+            (distance .>> ws)
+            (pchar '/' >>. ws >>. timeTransform)
+            (fun distance time -> distance / time 1.0)
+
+    type ConfigEdit = JobParameters -> JobParameters
+
+    let distanceEdit name edit =
+        pstringCI name >>. ws >>. pchar '=' >>. ws >>. distance |>> edit
+
+    let toolDiameter : Parser<ConfigEdit> =
+        distanceEdit "tool.diameter" (fun d job -> { job with Tool = { job.Tool with Diameter = d } })
